16b9ade7c1ec895a5fd168e1cedf565e36c0eba1
Sander Jochems <sanderjochems@hotmail.nl>
2022-03-29 16:02:59 +0200

    Update auth routes

--- gateway/routes/auth.js
+++ gateway/routes/auth.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const User = require('../models/user')
+const jwt = require('../services/token')
+const createError = require('http-errors')
 
 const router = express.Router()
 
@@ -17,12 +20,72 @@ router.get('/login', function (req, res) {
   })
 })
 
-router.post('/register', function (req, res) {
+router.post('/register', function (req, res, next) {
+  if (req.body.email === undefined || req.body.password === undefined) {
+    next(createError(400))
+    return
+  }
 
+  User.findOne({
+    email: req.body.email
+  })
+    .exec()
+    .then(value => {
+      if (value) {
+        next(createError(400, 'Email already taken'))
+      } else {
+        const user = new User(req.body)
+        user.save()
+          .then(value => {
+            res.json(tokenResponse(generateToken(value)))
+          }).catch(reason => {
+          next(createError(500, reason.message))
+        })
+      }
+    })
+    .catch(reason => {
+      next(createError(500, reason.message))
+    })
 })
 
-router.post('/login', function (req, res) {
+router.post('/login', function (req, res, next) {
+  if (req.body.email === undefined || req.body.password === undefined) {
+    next(createError(400))
+    return
+  }
 
+  User.findOne({
+    email: req.body.email,
+    password: req.body.password
+  })
+    .exec()
+    .then(value => {
+      if (value) {
+        res.json(tokenResponse(generateToken(value)))
+      } else {
+        next(createError(403, 'Invalid credentials'))
+      }
+    })
+    .catch(reason => {
+      next(createError(500, reason.message))
+    })
 })
+
+function generateToken (user) {
+  const payload = {
+    email: user.email
+  }
+  const options = {
+    subject: user._id.toString(),
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, options)
+}
+
+function tokenResponse (token) {
+  return {
+    token
+  }
+}
 
 module.exports = router
