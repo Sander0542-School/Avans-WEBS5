c39295e741af6e06fcd983b1607e0ba1180d1727
Sander Jochems <sanderjochems@hotmail.nl>
2022-03-31 23:07:26 +0200

    Add Docker to gateway

--- gateway/routes/cqrs.js
+++ gateway/routes/cqrs.js
@@ -4,7 +4,7 @@ const Forwarder = require('../services/forwarder')
 
 function initialize (passport) {
   const router = express.Router()
-  const breaker = CircuitBreaker(process.env.CQRS_HOST || 'localhost', process.env.CQRS_PORT || 3000, process.env.CQRS_API_KEY)
+  const breaker = CircuitBreaker(process.env.CQRS_HOST || 'cqrs', process.env.CQRS_PORT || 3000, process.env.CQRS_API_KEY)
   const forwarder = new Forwarder(router, breaker, passport)
 
   forwarder.get('/targets', '/')
