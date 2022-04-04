95975efbe40e05352b8a7e9ef9399abb294e950b
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-04 16:38:27 +0200

    Update

--- tagger/app.js
+++ tagger/app.js
@@ -1,1 +1,29 @@
 const { RabbitMQ } = require('avans-common')
+const Imagga = require('./services/imagga')
+
+const imagga = new Imagga()
+
+RabbitMQ()
+  .then(connection => {
+    connection.createChannel()
+      .then(channel => {
+        const exchange = 'webs.tagger'
+        channel.assertExchange(exchange, 'direct')
+
+        channel.assertQueue('', {
+          exclusive: true
+        })
+          .then(queue => {
+            channel.bindQueue(queue.queue, exchange, 'request')
+            channel.consume(queue.queue, async (message) => {
+              console.log('Message received')
+              content = JSON.parse(message.content.toString())
+              const tags = await imagga.tag(content.image)
+              channel.publish(exchange, content.return, Buffer.from(JSON.stringify(tags)))
+            })
+          })
+      })
+  })
+  .catch(error => {
+    console.error(error)
+  })
