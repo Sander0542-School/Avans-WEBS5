7f740df984174b8b5fabcda8f7b9b4ffa12403ec
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-05 23:54:27 +0200

    Update rabbitmq.js

--- common/connections/rabbitmq.js
+++ common/connections/rabbitmq.js
@@ -5,11 +5,17 @@ module.exports = function (host, port) {
   port = port || process.env.RABBITMQ_PORT || '5672'
 
   return new Promise((resolve, reject) => {
-    amqp.connect(`amqp://${host}:${port}`).then(connection => {
-      resolve(connection)
-      console.log(`AMQP connection to ${host}:${port} opened`)
-    }).catch(error => {
-      reject(error)
+    amqp.connect({
+      protocol: 'amqp',
+      hostname: host,
+      port: port
     })
+      .then(connection => {
+        resolve(connection)
+        console.log(`AMQP connection to ${host}:${port} opened`)
+      })
+      .catch(error => {
+        reject(error)
+      })
   })
 }
