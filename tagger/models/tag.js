86fd4ff013826ef93965e8fefbe0397a46689661
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-05 11:14:52 +0200

    Update tag.js

--- tagger/models/tag.js
+++ tagger/models/tag.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
   hash: {
     type: String,
-    required: true,
+    required: true
   },
   tags: {
     type: mongoose.Schema.Types.Map,
     required: true
-  },
+  }
 })
 
 module.exports = mongoose.model('Tag', schema)
