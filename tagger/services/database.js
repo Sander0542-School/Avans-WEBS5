9772419c496357d3b9ee91d9d279b821ab418449
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-07 13:39:22 +0200

    Add logging to database.js

--- tagger/services/database.js
+++ tagger/services/database.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose')
 class Database {
   static initialize () {
     mongoose.connect(process.env.TAGGER_MONGO_URL)
+      .then(() => {
+        console.log('Connected to MongoDB')
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }
 }
 
