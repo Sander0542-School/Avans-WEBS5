f5d72b1d1e62341da2ad047a8eefd3ad8a04b71f
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-08 16:55:59 +0200

    Use async in submission\services\consumer

--- submission/services/consumer.js
+++ submission/services/consumer.js
@@ -3,50 +3,40 @@ const Submission = require('../models/submission')
 
 function initialize (rabbitMqConnection) {
   rabbitMqConnection.createChannel()
-    .then(channel => {
-      channel.assertQueue('webs.submission.target', {
-        durable: true
+    .then(async channel => {
+      const targetQueue = await channel.assertQueue('webs.submission.target', { durable: true })
+      channel.consume(targetQueue.queue, async message => {
+        try {
+          const content = JSON.parse(message.content.toString())
+
+          await Target.create(content)
+          console.log(`Target ${target._id} created`)
+
+          channel.ack(message)
+        } catch (error) {
+          console.log(error)
+        }
       })
-        .then(queue => {
-          channel.consume(queue.queue, message => {
-            const content = JSON.parse(message.content.toString())
 
-            Target.create(content)
-              .then(target => {
-                console.log(`Target ${target._id} created`)
-                channel.ack(message)
-              })
-              .catch(error => {
-                console.log(error)
-              })
-          })
-        })
+      const tagsQueue = await channel.assertQueue('webs.submission.tags', { durable: true })
+      channel.consume(tagsQueue.queue, async message => {
+        try {
+          const content = JSON.parse(message.content.toString())
 
-      channel.assertQueue('webs.submission.tags', {
-        durable: true
+          const submission = await Submission.findByIdAndUpdate(content.documentId, { tags: content.tags }, { returnDocument: 'after' })
+          console.log(`Updated submission ${submission._id} with ${content.tags.count} tags`)
+
+          const cqrsQueue = await channel.assertQueue('webs.cqrs.submission', { durable: true })
+          channel.sendQueue(cqrsQueue.queue, Buffer.from(JSON.stringify({
+            action: 'create',
+            data: submission
+          })))
+
+          channel.ack(message)
+        } catch (error) {
+          console.log(error)
+        }
       })
-        .then(queue => {
-          channel.consume(queue.queue, message => {
-            const content = JSON.parse(message.content.toString())
-
-            Submission.findByIdAndUpdate(content.documentId, { tags: content.tags }, { returnDocument: 'after' })
-              .then(submission => {
-                console.log(`Updated submission ${submission._id} with ${content.tags.count} tags`)
-                channel.ack(message)
-
-                channel.assertQueue('webs.cqrs.submission', { durable: true })
-                  .then(queue => {
-                    channel.sendToQueue(queue.queue, Buffer.from(JSON.stringify({
-                      action: 'create',
-                      data: submission
-                    })))
-                  })
-              })
-              .catch(error => {
-                console.error(error)
-              })
-          })
-        })
     })
 }
 
