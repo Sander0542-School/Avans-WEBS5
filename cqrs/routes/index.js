a5b8c07db344527ee4b36653164a80f8c7cc0792
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-08 17:39:46 +0200

    Replace promises with await

--- cqrs/routes/index.js
+++ cqrs/routes/index.js
@@ -5,119 +5,114 @@ const Submission = require('../models/submission')
 const Target = require('../models/target')
 
 router.get('/targets', async function (req, res, next) {
-  const page = req.query.page || 0
+  try {
+    const page = req.query.page || 0
 
-  Target.paginate({}, {
-    page: page,
-    limit: 10,
-    customLabels: {
-      docs: 'targets'
-    }
-  })
-    .then(result => {
-      res.json(result)
+    const targets = await Target.paginate({}, {
+      page: page,
+      limit: 10,
+      customLabels: {
+        docs: 'targets'
+      }
     })
-    .catch(error => {
-      next(createError(500, error.message))
-    })
+
+    res.json(targets)
+  } catch (error) {
+    next(createError(500, error.message))
+  }
 })
 
 router.get('/targets/:id', async function (req, res, next) {
-  Target.findById(req.params.id)
-    .then(target => {
-      if (!target) {
-        next(createError(404, 'Target not found'))
-      } else {
-        res.json(target)
-      }
-    })
-    .catch(error => {
-      next(createError(500, error.message))
-    })
+  try {
+    const target = await Target.findById(req.params.id)
+
+    if (!target) {
+      next(createError(404, 'Target not found'))
+    }
+
+    res.json(target)
+  } catch (error) {
+    next(createError(500, error.message))
+  }
 })
 
 router.get('/targets/:id/image', async function (req, res, next) {
-  Target.findById(req.params.id)
-    .select('image')
-    .then(target => {
-      if (!target) {
-        next(createError(404, 'Target not found'))
-      } else {
-        res.send(target.image)
-      }
-    })
-    .catch(error => {
-      next(createError(500, error.message))
-    })
+  try {
+    const target = await Target.findById(req.params.id).select('image')
+
+    if (!target) {
+      next(createError(404, 'Target not found'))
+    }
+
+    res.send(target.image)
+  } catch (error) {
+    next(createError(500, error.message))
+  }
 })
 
 router.get('/targets/:id/submissions', async function (req, res, next) {
-  const page = req.query.page || 0
+  try {
+    const page = req.query.page || 0
+    const target = await Target.findById(req.params.id)
 
-  Target.findById(req.params.id)
-    .then(target => {
-      if (!target) {
-        next(createError(404, 'Target not found'))
-      } else {
-        return Submission.paginate({
-          targetId: target._id
-        }, {
-          page: page,
-          limit: 10,
-          customLabels: {
-            docs: 'submissions'
-          }
-        })
+    if (!target) {
+      next(createError(404, 'Target not found'))
+    }
+
+    const submissions = await Submission.paginate({
+      targetId: target._id
+    }, {
+      page: page,
+      limit: 10,
+      customLabels: {
+        docs: 'submissions'
       }
     })
-    .then(result => {
-      res.json(result)
-    })
-    .catch(error => {
-      next(createError(500, error.message))
-    })
+
+    res.json(submissions)
+  } catch (error) {
+    next(createError(500, error.message))
+  }
 })
 
 router.get('/targets/:id/submissions/:submissionId', async function (req, res, next) {
-  Target.findById(req.params.id)
-    .then(target => {
-      if (!target) {
-        next(createError(404, 'Target not found'))
-      } else {
-        return Submission.findById(req.params.submissionId)
-      }
-    })
-    .then(submission => {
-      if (!submission) {
-        next(createError(404, 'Submission not found'))
-      } else {
-        res.json(submission)
-      }
-    })
-    .catch(error => {
-      next(createError(500, error.message))
-    })
+  try {
+    const target = await Target.findById(req.params.id)
+
+    if (!target) {
+      next(createError(404, 'Target not found'))
+    }
+
+    const submission = await Submission.findById(req.params.submissionId)
+
+    if (!submission) {
+      next(createError(404, 'Submission not found'))
+    }
+
+    res.json(submission)
+  } catch (error) {
+    next(createError(500, error.message))
+  }
 })
 
 router.get('/targets/:id/submissions/:submissionId/image', async function (req, res, next) {
-  Target.findById(req.params.id)
-    .then(target => {
-      if (!target) {
-        next(createError(404, 'Target not found'))
-      } else {
-        return Submission.findById(req.params.submissionId).select('image')
-      }
-    })
-    .then(submission => {
-      if (!submission) {
-        next(createError(404, 'Submission not found'))
-      } else {
-        res.send(submission.image)
-      }
-    })
-    .catch(error => {
-      next(createError(500, error.message))
-    })
+  try {
+    const target = await Target.findById(req.params.id)
+
+    if (!target) {
+      next(createError(404, 'Target not found'))
+    }
+
+    const submission = await Submission.findById(req.params.submissionId).select('image targetId')
+
+    if (!submission || submission.targetId !== target._id) {
+      next(createError(404, 'Submission not found'))
+    }
+
+    res.send(submission.image)
+  } catch (error) {
+    next(createError(500, error.message))
+  }
 })
 
 module.exports = router
