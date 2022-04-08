b17ca8b765a26f000fba0ba9c315135a66acd241
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-08 13:25:59 +0200

    Update tagger

--- tagger/models/tag.js
+++ tagger/models/tag.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-  hash: {
-    type: String,
+  targetId: {
+    type: mongoose.Schema.Types.ObjectId,
     required: true
   },
   tags: {
