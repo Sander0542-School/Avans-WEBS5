fb4be0fa9f0e43a09de2683a6a546beb059339b2
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-08 16:04:13 +0200

    Update json body limit

--- cqrs/app.js
+++ cqrs/app.js
@@ -17,7 +17,7 @@ const indexRouter = require('./routes/index')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 app.use(Prometheus)
--- gateway/app.js
+++ gateway/app.js
@@ -30,7 +30,7 @@ roles.use('owner', req => {
 })
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 app.use(passport.initialize())
